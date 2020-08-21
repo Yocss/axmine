aff19fd3650d416798d284d3fa03e200da9190fe
Yocss <yeahoor@qq.com>
2020-08-21 15:55:17 +0800

    add store function

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@
 import { formatDate } from './utils/formatDate'
 // get variable's type
 import { getType } from './utils/getType'
+// cookie
+import Store from './utils/store'
 
-export default { formatDate, getType }
+export default { formatDate, getType, store: new Store() }
