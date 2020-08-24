2250c15f7abc9bf9e2967ae34d5891246557ee46
Yocss <yeahoor@qq.com>
2020-08-24 15:39:16 +0800

    增加validate测试

--- test/helper.test.js
+++ test/helper.test.js
@@ -27,3 +27,168 @@ test('Test getType(() => {}): should be \'function\'', () => {
 // test('Test store.set()', () => {
 //   expect(store.set('key1', 111).toBe(false))
 // })
+
+// test validate
+const args = [
+  {
+    name: 'Test validate required',
+    rules: { a: { required: true, message: '不能为空' } },
+    form: { a: '' },
+    res: {
+      status: false,
+      infos: [
+        { message: '不能为空', key: 'a' }
+      ]
+    }
+  },
+  {
+    name: 'Test validate string\'s len',
+    rules: { a: { len: 3, message: '字符长度必须为3' } },
+    form: { a: '1' },
+    res: {
+      status: false,
+      infos: [
+        { message: '字符长度必须为3', key: 'a' }
+      ]
+    }
+  },
+  {
+    name: 'Test validate array\'s len',
+    rules: { a: { len: 3, message: '数组长度必须为3' } },
+    form: { a: [1,2] },
+    res: {
+      status: false,
+      infos: [
+        { message: '数组长度必须为3', key: 'a' }
+      ]
+    }
+  },
+  {
+    name: 'Test validate number: min and max 1',
+    rules: { a: { min: 3, max: 10, message: '3 <= a <= 10' } },
+    form: { a: 2 },
+    res: {
+      status: false,
+      infos: [
+        { message: '3 <= a <= 10', key: 'a' }
+      ]
+    }
+  },
+  {
+    name: 'Test validate number: min and max 2',
+    rules: { a: { min: 3, max: 10, message: '3 <= a <= 10' } },
+    form: { a: 7 },
+    res: {
+      status: true,
+      infos: []
+    }
+  },
+  {
+    name: 'Test validate !number: min and max 1',
+    rules: { a: { min: 3, max: 10, message: '长度介于3-10位' } },
+    form: { a: '2' },
+    res: {
+      status: false,
+      infos: [
+        { message: '长度介于3-10位', key: 'a' }
+      ]
+    }
+  },
+  {
+    name: 'Test validate !number: min and max 2',
+    rules: { a: { min: 3, max: 10, message: '长度介于3-10位' } },
+    form: { a: [1,2,3,4] },
+    res: {
+      status: true,
+      infos: []
+    }
+  },
+  {
+    name: 'Test validate enum 1',
+    rules: { a: { enum: ['aa', 'bb', 'cc'], message: '3 <= a <= 10' } },
+    form: { a: 'ab' },
+    res: {
+      status: false,
+      infos: [
+        { message: '3 <= a <= 10', key: 'a' }
+      ]
+    }
+  },
+  {
+    name: 'Test validate enum 2',
+    rules: { a: { enum: ['aa', 'bb', 'cc'], message: '3 <= a <= 10' } },
+    form: { a: 'aa' },
+    res: {
+      status: true,
+      infos: []
+    }
+  },
+  {
+    name: 'Test validate type 1',
+    rules: { a: { type: 'number', message: '3 <= a <= 10' } },
+    form: { a: 1 },
+    res: {
+      status: true,
+      infos: []
+    }
+  },
+  {
+    name: 'Test validate type 2',
+    rules: { a: { type: 'number', message: 'must be number' } },
+    form: { a: '1' },
+    res: {
+        status: false,
+        infos: [{ message: 'must be number', key: 'a' }]
+    }
+  },
+  {
+    name: 'Test validate reg 1',
+    rules: { a: { pattern: /abc/i, message: 'error' } },
+    form: { a: 'abCde' },
+    res: {
+        status: true,
+        infos: []
+    }
+  },
+  {
+    name: 'Test validate reg 2',
+    rules: { a: { pattern: /abc/, message: 'error' } },
+    form: { a: 'adcde' },
+    res: {
+        status: false,
+        infos: [{ message: 'error', key: 'a' }]
+    }
+  },
+  {
+    name: 'Test validate reg 3',
+    rules: { a: { pattern: /^\d+$/, message: 'error' } },
+    form: { a: '123' },
+    res: {
+        status: true,
+        infos: []
+    }
+  },
+  {
+    name: 'Test validate validator',
+    rules: { a: { validator: v => v > 10, message: 'error' } },
+    form: { a: '123' },
+    res: {
+        status: true,
+        infos: []
+    }
+  },
+  {
+    name: 'Test validate mix',
+    rules: { pass: { min: 6, required: true, max: 18, message: 'error' } },
+    form: { pass: 'abc123' },
+    res: {
+        status: true,
+        infos: []
+    }
+  },
+]
+for (let i = 0; i < args.length; i++) {
+  test(args[i].name || `Test validate() ${i}`, () => {
+    expect(helper.validate(args[i].rules, args[i].form)).toEqual(args[i].res)
+  })
+}
